3637f04dfc1e738e81d3893adb8eacef056ae693
Melo at Github <106290334+Melo-Professional@users.noreply.github.com>
2025-12-18 15:52:46 -0300

    Add 'kid' to the list of user keys

--- js/app.js
+++ js/app.js
@@ -66,7 +66,7 @@ const sensitiveKeys = new Set([
     "syslogauth", "dbRecordsEncryptKey", "dbRecordsDecryptKey",
     "dbEncryptKey", "certificatePrivateKeyPassword", "id",
     "newMebxPassword", "username", "from", "newAccountsUserGroups",
-    "agentCoreDumpUsers","hmackey"
+    "agentCoreDumpUsers", "kid", "hmackey"
 ]);
 
 const userGroupKeys = ["manageAllDeviceGroups", "manageCrossDomain", "adminAccounts", "InterUserMessaging"];
